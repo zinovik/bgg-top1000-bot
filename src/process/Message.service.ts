5a1aec71b4368e99263d678198d303ddcd189e59
Max Zinovik <zinovik@gmail.com>
2023-12-22 16:12:14 +0100

    Increase minimal change, remove some text

--- src/process/Message.service.ts
+++ src/process/Message.service.ts
@@ -3,6 +3,8 @@ import { Data } from '../common/model/Data.interface';
 
 type Game = { rank: number; name: string; year: string };
 
+const MINIMAL_CHANGE = 2;
+
 export class MessageService implements ProcessService {
   formatMessage({ newData, oldData }: { newData: Data; oldData: Data }): string {
     const date = this.getDateString('On date', newData.date);
@@ -25,8 +27,8 @@ export class MessageService implements ProcessService {
 
       const change = oldGame.rank - game.rank;
 
-      if (change > 1) increasedGames[change] = [...(increasedGames[change] || []), game];
-      if (change < -1) decreasedGames[change] = [...(decreasedGames[change] || []), game];
+      if (change > MINIMAL_CHANGE) increasedGames[change] = [...(increasedGames[change] || []), game];
+      if (change < 0 - MINIMAL_CHANGE) decreasedGames[change] = [...(decreasedGames[change] || []), game];
     });
 
     const newGamesString = this.getAdditionalList('🆕 Game(s) new in Top 1000', newGames);
@@ -34,19 +36,15 @@ export class MessageService implements ProcessService {
 
     const increasedGamesStrings = Object.keys(increasedGames)
       .sort((change1, change2) => Number(change2) - Number(change1))
-      .map((change) =>
-        this.getAdditionalList(`⬆️ ${change} positions up`, increasedGames[change]),
-      );
+      .map((change) => this.getAdditionalList(`⬆️ ${change}`, increasedGames[change]));
 
     const decreasedGamesStrings = Object.keys(decreasedGames)
       .sort((change1, change2) => Number(change1) - Number(change2))
-      .map((change) =>
-        this.getAdditionalList(`⬇️ ${Math.abs(Number(change))} positions down`, decreasedGames[change]),
-      );
+      .map((change) => this.getAdditionalList(`⬇️ ${Math.abs(Number(change))}`, decreasedGames[change]));
 
-    return `${date}\n\n${oldDate}\n\n\n${newGamesString}\n\n${droppedGamesString}\n\n\n${increasedGamesStrings.join(
+    return `${date}\n\n${oldDate}\n\n\n${newGamesString}\n\n${droppedGamesString}\n\nPositions up\n${increasedGamesStrings.join(
       '\n\n',
-    )}\n\n\n${decreasedGamesStrings.join('\n\n')}`;
+    )}\n\nPositions down\n${decreasedGamesStrings.join('\n\n')}`;
   }
 
   private getDateString(text: string, date: string): string {
